b66d25c254f92c43a75290276a596a00b0fcfbbe
Asjal Amir <mohdasjal1@gmail.com>
2024-10-31 13:15:53 +0500

    upload path changed

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -15,7 +15,7 @@ import path from 'path';
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/'); // Set the upload directory
+        cb(null, 'test/data/'); // Set the upload directory
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + path.extname(file.originalname)); // Set the file name
