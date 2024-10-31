45a0e765765ea5d1f0404250ad5694c7c574e73e
Asjal Amir <mohdasjal1@gmail.com>
2024-10-31 23:55:26 +0500

    05-versions-space.pdf added

--- src/utils/filereader.js
+++ src/utils/filereader.js
@@ -22,15 +22,15 @@ import pdfParse from 'pdf-parse';
 import mammoth from 'mammoth';
 
 export async function readFileContent(filePath, mimeType) {
-  const path = './test/data/05-versions-space.pdf';
+//   const path = './test/data/05-versions-space.pdf';
 
-if (fs.existsSync(path)) {
-  const data = fs.readFileSync(path);
-  // Process the file as needed
-} else {
-  console.warn(`File not found: ${path}`);
-  // Handle the missing file scenario
-}
+// if (fs.existsSync(path)) {
+//   const data = fs.readFileSync(path);
+//   // Process the file as needed
+// } else {
+//   console.warn(`File not found: ${path}`);
+//   // Handle the missing file scenario
+// }
   
 
   // Check if the file exists before reading
